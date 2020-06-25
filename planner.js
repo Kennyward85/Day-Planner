4b832654c7f968392a3a6a0c701020cd9823e8c8
Osyrus85 <osyrus85@gmail.com>
2020-06-25 16:14:56 -0500

    Fixed issue with moment working

--- planner.js
+++ planner.js
@@ -1,44 +1,42 @@
-var currentDay = moment().format("dddd, MMM, Da");
+// Sets up moment for jumbotron 
+var currentDay = moment().format("dddd, MMMM Do");
 var displayMoment = document.getElementById("currentDay");
-displayMoment.innerHTML = currentDay
+displayMoment.innerHTML = currentDay;
 
 
-$(document).ready(function () {
-
+$(document).ready(function() {
+// Sets up var to store the information  for document
     var value = $(this).siblings(".description").val();
 
     var time = $(this).parent().attr("id");
 
     localStorage.setItem(time, value);
-
-    function hourUpdater() {
+// this function updates the css with moment so that the colors reflect properly for time past present future
+    function hourUpdate() {
         var timeNow = moment().hours();
         $(".time-block").each(function () {
-            var blockHour = parseInt(this).attr("id").split("-")[1];
-
-        if (blockhour < timenow) {
-            $(this).addClass(".past");
+            var divHour = parseInt($(this).attr("id").split("-")[1]);
+                
+        if (divHour < timeNow) {
+            $(this).addClass("past");
         }
-        else if (blockHour === timeNow) {
-            $(this).removeClass(".past");
-            $(this).addClass(".present");
+        else if (divHour === timeNow) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
         }
         else {
-            $(this).removeClass(".past");
-            $(this).removeClass(".present");
-            $(this).addClass(".future");
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");  
         }
     });
     
 };
 
-hourUpdater();
+hourUpdate();
 
-
-var interval = setInterval(hourUpdater, 15000);
-
-
-
+  var interval = setInterval(hourUpdate, 15000);
+// sets up save button function  for the time and value 
 $(".saveBtn").on("click", function () {
     var value = $(this).siblings(".description").val();
 
@@ -49,23 +47,14 @@ $(".saveBtn").on("click", function () {
 });
 
 
-
-
-
-
-
-
-
-
-
-$("hour-8 .description").val(localStorage.getItem("hour-8"));
-$("hour-9 .description").val(localStorage.getItem("hour-9"));
-$("hour-10 .description").val(localStorage.getItem("hour-10"));
-$("hour-11 .description").val(localStorage.getItem("hour-11"));
-$("hour-12 .description").val(localStorage.getItem("hour-12"));
-$("hour-13 .description").val(localStorage.getItem("hour-13"));
-$("hour-14 .description").val(localStorage.getItem("hour-14"));
-$("hour-15 .description").val(localStorage.getItem("hour-15"));
-$("hour-16 .description").val(localStorage.getItem("hour-16"));
-$("hour-17 .description").val(localStorage.getItem("hour-17"));
+$("#hour-8 .description").val(localStorage.getItem("hour-8"));
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
 });
