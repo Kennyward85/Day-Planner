e24e396f8a719e75e2dfa4b213824653fd7312f4
Osyrus85 <osyrus85@gmail.com>
2020-06-25 19:35:52 -0500

    Final Push

--- README.md
+++ README.md
@@ -6,22 +6,22 @@ Created a simple calendar application that allows the user to save events for ea
 ## Acceptance Criteria
 
 ```
-GIVEN I am using a daily planner to create a schedule
-WHEN I open the planner
-THEN the current day is displayed at the top of the calendar
-WHEN I scroll down
-THEN I am presented with timeblocks for standard business hours
-WHEN I view the timeblocks for that day
-THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-WHEN I click into a timeblock
-THEN I can enter an event
-WHEN I click the save button for that timeblock
-THEN the text for that event is saved in local storage
-WHEN I refresh the page
-THEN the saved events persist
+1. Create daily planner to set up a schedule for typical working hours 
+
+Created a planner that populates the proper day when opening the webpage based off of Js moment.
+Used a timeframe of 8-5 for the planner itself for the columns 
+
+2. Set up a moment function to color code the timeblocks based off time of day.With this it color coded if the time had passed or if it was in that hour or if it was still upcoming 
+
+3. In these timeblocks you can enter events for that time frame and click the save button storing it in the local storage
+
+4. with this information even if you refresh the page it will still display the information for the local storage 
+
+5. Set up an intervale for it to dynamically update ever minute 
 ```
 
 
+https://kennyward85.github.io/Day-Planner/.
 
 ![Day Planner](https://user-images.githubusercontent.com/66036794/85795473-ec8f0b00-b6fd-11ea-967a-21d62a832af0.png)
 
--- planner.js
+++ planner.js
@@ -1,18 +1,15 @@
 // Sets up moment for jumbotron 
 var currentDay = moment().format("dddd, MMMM Do");
-var displayMoment = document.getElementById("currentDay");
-displayMoment.innerHTML = currentDay;
-
+var display = document.getElementById("currentDay");
+display.innerHTML = currentDay;
 
 $(document).ready(function() {
-// Sets up var to store the information  for document
+// Sets up var to store the information  for document so it can pull to the function for the color coding 
     var value = $(this).siblings(".description").val();
-
     var time = $(this).parent().attr("id");
-
     localStorage.setItem(time, value);
 // this function updates the css with moment so that the colors reflect properly for time past present future
-    function hourUpdate() {
+    function update() {
         var timeNow = moment().hours();
         $(".time-block").each(function () {
             var divHour = parseInt($(this).attr("id").split("-")[1]);
@@ -30,23 +27,18 @@ $(document).ready(function() {
             $(this).addClass("future");  
         }
     });
-    
 };
+update();
 
-hourUpdate();
-
-  var interval = setInterval(hourUpdate, 15000);
+  var interval = setInterval(update, 60000);
 // sets up save button function  for the time and value 
 $(".saveBtn").on("click", function () {
     var value = $(this).siblings(".description").val();
-
     var time = $(this).parent().attr("id");
-
     localStorage.setItem(time, value);
-
 });
-
-
+//  set up for it to getitem from local storage for every hour was not sure if there is a more
+//  condensed way this can be done to not have as many lines of code 
 $("#hour-8 .description").val(localStorage.getItem("hour-8"));
 $("#hour-9 .description").val(localStorage.getItem("hour-9"));
 $("#hour-10 .description").val(localStorage.getItem("hour-10"));
